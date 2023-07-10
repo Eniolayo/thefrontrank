2e0c47d071abadb2fe6fe0346f428d7be64de33f
Eniolayo <eniolayodeji@gmail.com>
2023-07-10 14:57:40 +0100

    aoe : favicon changed

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,8 +2,9 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `thefrontrank`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `The Front-Rank`,
+    description: `Stay updated with the latest trends, news, and expert opinions through our informative blog. Delve into engaging content designed to educate and entertain.`,
+    author: `@ayodejiikujuni`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
@@ -12,11 +13,24 @@ const config: GatsbyConfig = {
   plugins: [
     "gatsby-plugin-postcss",
     "gatsby-plugin-image",
-    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-sitemap",
+      options: {
+        output: "/sitemap.xml",
+      },
+    },
     {
       resolve: "gatsby-plugin-manifest",
       options: {
         icon: "src/images/icon.png",
+      },
+    },
+    {
+      resolve: "gatsby-plugin-robots-txt",
+      options: {
+        host: "https://thefrontrankmaster.gatsbyjs.io/",
+        sitemap: "https://thefrontrankmaster.gatsbyjs.io/sitemap.xml",
+        policy: [{ userAgent: "*", allow: "/" }],
       },
     },
     "gatsby-plugin-mdx",
